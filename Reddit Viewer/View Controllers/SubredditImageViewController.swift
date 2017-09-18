8973636b0be92b54a5590c46f7265b3f12fd0cf9
Duncan <buzzbuzz.macdonald@gmail.com>
2017-09-18 15:33:06 -0700

    Cool
    wow

---
+++ Reddit Viewer/View Controllers/SubredditImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SubredditImageViewController.swift
+//  Reddit Viewer
+//
+//  Created by Duncan MacDonald on 9/18/17.
+//
+
+import Foundation
+import UIKit
+
+class SubredditImageViewController: UIViewController {
+    
+}
---
+++ Reddit Viewer/Views/ThumbnailCell.swift
@@ -0,0 +1,14 @@
+//
+//  ThumbnailCell.swift
+//  Reddit Viewer
+//
+//  Created by Duncan MacDonald on 9/18/17.
+//
+
+import Foundation
+import UIKit
+
+class ThumbnailCell: UICollectionViewCell {
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    
+}
